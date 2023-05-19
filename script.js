21e64222d0ad5f2371ba1a44e219ed9df057806e
Ramsucclets <68151701+Ramsucclets@users.noreply.github.com>
2023-05-19 22:37:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// You can use JavaScript to dynamically update the link or perform other actions
-// For example:
-// document.getElementById('discord-link').href = 'YOUR_DYNAMIC_DISCORD_INVITE_LINK';
-
 // Add some interactivity
-document.getElementById('discord-link').addEventListener('mouseover', function() {
+document.getElementById('discord-button').addEventListener('mouseover', function() {
     this.innerText = 'Click to join!';
 });
 
-document.getElementById('discord-link').addEventListener('mouseout', function() {
+document.getElementById('discord-button').addEventListener('mouseout', function() {
     this.innerText = 'Join our Discord server';
 });
 
 document.getElementById('discord-button').addEventListener('click', function() {
-    window.location.href = 'https://discord.gg/kAuTCVa5Bc';
+    window.location.href = 'YOUR_DISCORD_INVITE_LINK';
 });
+
